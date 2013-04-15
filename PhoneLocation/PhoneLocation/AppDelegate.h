9336d494233fdf07c3cbeef7f656e8bf2522d07a
zhnana <atzhnana@gmail.com>
2013-04-15 10:35:22 +0800

    新建项目

---
+++ PhoneLocation/PhoneLocation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PhoneLocation
+//
+//  Created by NanaZhang on 4/15/13.
+//  Copyright (c) 2013 NanaZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PhoneLocation/PhoneLocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhoneLocation
+//
+//  Created by NanaZhang on 4/15/13.
+//  Copyright (c) 2013 NanaZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
